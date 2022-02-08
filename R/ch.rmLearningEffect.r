5b04fff40da61383847dfce567c4c5def984ea9a
DJC <dalejcohen@gmail.com>
2022-02-08 14:37:20 -0500

    added some new functions

---
+++ R/ch.rmLearningEffect.r
@@ -0,0 +1,23 @@
+
+#' A function to remove a learning function from data
+#'
+#' This function attempts to fit a non-linear, decellerating function to the specifed data.  Then it adds two columns to the dataframe: a column with the predicted datapoints based on the nls fit and a column with the residuals around that fit. The function also returns the nls fit object
+#' @param data the dataframe containing the data..
+#' @param x a string the specifies the column name in "data" that contains the x variable.
+#' @param y a string the specifies the column name in "data" that contains the y variable.
+#' @param fitCol a string that specifies the name of the new column that will contain the predicted datapoints.
+#' @param resCol a string that specifies the name of the new column that will contain the residual datapoints.
+#' @keywords morals learning function remove
+#' @return a list containing: data = original datafram with the new columns; nlsFit = the nls fit object.
+#' @export
+#' @examples ch.moralsRmLearningEffect (data=moralsData,"trial", "RT", "fitRT", "resRT")
+
+ch.rmLearningEffect <- function (data, x, y, fitCol, resCol, a=1, b=-1, c=1) {
+
+		fit.nls <- ch.getLearningEffect(data, x,y, a = a, b = b, c = c)
+
+		data[[resCol]] <- resid(fit.nls)
+		data[[fitCol]] <- fitted(fit.nls)
+
+		return(list(data = data, nlsFit = fit.nls))
+}
